dd21078b61316d917adaf6a97bec89acbab31e8c
AmirhossseinBabaei <ab8622822@gmail.com>
2026-02-03 17:31:38 +0330

    pushing directories and composer json files

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'laraport/laraport',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bbaf05af757380a5b0a10eb6179917599ea3a845',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'laraport/laraport' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bbaf05af757380a5b0a10eb6179917599ea3a845',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
