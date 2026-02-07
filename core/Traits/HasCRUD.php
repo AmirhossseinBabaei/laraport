ce93c4cbcd942b19c72aefe27c79621e08ba4adc
AmirhossseinBabaei <ab8622822@gmail.com>
2026-02-07 19:20:00 +0330

    adding collection class to returned data from database

--- core/Traits/HasCRUD.php
+++ core/Traits/HasCRUD.php
@@ -5,16 +5,19 @@ declare(strict_types=1);
 namespace Core\Traits;
 
 use Core\Database\Database;
+use http\Exception\UnexpectedValueException;
+
+require_once "core/Database/Database.php";
 
 trait HasCRUD
 {
-    public function setFillables(array $array): string
+
+    protected function setFillables(array $array): string
     {
         $fillable = [];
-
-        foreach ($this->fillables as $attribute) {
-            $fillable = $attribute . " = ? ";
-            $this->setValues(['key' => $attribute, $array[$attribute]]);
+        foreach ($this->fillable as $attribute) {
+            $fillable[] = $attribute . " = ? ";
+            $this->setValues($attribute, $array[$attribute]);
         }
 
         return implode(',', $fillable);
@@ -22,11 +25,11 @@ trait HasCRUD
 
     public function insert(array $data)
     {
-        $this->setSql("INSERT INTO $this->table SET $this->fillables($data)" . $this->createdAt . " = Now();");
+        $this->setSql("INSERT INTO $this->table SET $this->setFillables($data)," . $this->created_at . " = Now();");
         $this->executeQuery();
         $this->resetQuery();
 
-        $object = $this->find((int)Database::newInsertId());
+        $object = $this->find(Database::newInsertId());
         $defaultVariables = get_class_vars(get_called_class());
         $allVariables = get_object_vars($object);
         $differentVariables = array_diff(array_keys($allVariables), array_keys($defaultVariables));
@@ -39,49 +42,59 @@ trait HasCRUD
         return $this;
     }
 
-    public function find(int $id)
+    protected function find($id)
     {
-        $this->setSql("SELECT * FROM $this->table");
-        $this->setWhere('AND', $this->primaryKey . " = ?");
-        $this->setValues($this->primaryKey, $id);
+        $this->setSql('SELECT * FROM ' . $this->table);
+        $this->setWhere('AND', $this->primaryKey . ' = ?');
+        $this->setValue($this->primaryKey, $id);
 
-        $statement = $this->exequteQuery();
+        $statement = $this->executeQuery();
         $data = $statement->fetch();
 
-        return $data;
+        if ($data) {
+            return $this->setAttribute($data);
+        } else {
+            return null;
+        }
     }
 
-    public function update(int $id, array $data): static
+    public function update(array $data, $id): static
     {
-        $this->setSql("UPDATE $this->table SET $this->setFillables($data)" . " " . $this->createdAt = " = Now()");
+        $this->setSql('UPDATE' . $this->table . "SET" . $this->setFillables($data) . ", $this->updatedAt" . " = Now()");
         $this->setWhere('AND', $this->primaryKey . " = ?");
-        $this->exequteQuery();
         $this->setValues($this->primaryKey, $id);
+        $this->executeQuery();
         $this->resetQuery();
-
         return $this;
-    }
-
-    public function delete(int $id): bool
-    {
-        $this->setSql("DELETE FROM $this->table");
-        $this->setWhere('AND', $this->primaryKey . " = ?");
-        $this->setValues($this->primaryKey, $id);
-        $statement = $this->exequteQuery();
-        $this->resetQuery();
-
-        return (bool)$statement;
     }
 
     public function getAll()
     {
-        $this->setSql("SELECT * FROM $this->table");
-        $this->setOrderBy($this->primaryKey, "DESC");
-        $statement = $this->exequteQuery();
+        $this->setSql("SELECT * FROM" . $this->table);
+        $statement = $this->executeQuery();
         $data = $statement->fetchAll();
+
+        if ($data) {
+            $this->setObject($data);
+            return $this->collection;
+        } else {
+            return null;
+        }
+    }
+
+    public function delete($id): bool
+    {
+        $this->setSql("DELETE FROM " . $this->table);
+        $this->setWhere("AND", $this->primaryKey . " = ?");
+        $this->setValues($this->primaryKey, $id);
+        $statement = $this->executeQuery();
         $this->resetQuery();
 
-        return $data ?? null;
+        if ($statement) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     public function where($attribute, $operator, $value): static
